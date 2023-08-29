1cadfaccb3a26c7d1950cf1e59c32c3bf26fac60
Ajmain Nishu <ajmainnishu@gmail.com>
2023-08-29 15:33:06 +0600

    feat: Log in page added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,7 +4,26 @@ import Form from 'react-bootstrap/Form';
 const Login = () => {
     return (
         <div className='w-50 border rounded mx-auto my-5'>
-            
+            <h2 className='text-center pt-4'>Log In</h2>
+            <Form className='py-4 px-5'>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name="password" placeholder="Password" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Log In
+                </Button>
+            </Form>
         </div>
     );
 };
