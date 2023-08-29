d21f9ca8699b0344ac1d0a64e289b9bed2e1931d
Ajmain Nishu <ajmainnishu@gmail.com>
2023-08-29 15:03:09 +0600

    feat: Main background pages added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import BackgroundPhoto from "./BackgroundPhoto/BackgroundPhoto";
 import LuxuryFurnished from "./LuxuryFurnished/LuxuryFurnished";
 
 const Home = () => {
     return (
         <div>
             <LuxuryFurnished></LuxuryFurnished>
+            <BackgroundPhoto></BackgroundPhoto>
         </div>
     );
 };
